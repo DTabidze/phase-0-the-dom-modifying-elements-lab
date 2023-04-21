7d315f3204d266eeb9f4122f7781ee4875692019
Davit <davidtabidzef1@gmail.com>
2023-04-21 23:32:26 +0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainID = document.getElementById("main");
+mainID.remove();
+const newHeader = document.createElement("h1");
+//headerOne.textContent = "newHeader";
+newHeader.id = "victory";
+//const newHeaderID = document.getElementById("victory");
+newHeader.textContent = "Davit is the champion";
+document.body.append(newHeader);
